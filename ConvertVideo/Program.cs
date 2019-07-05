4ad25a64fc5f19119eb9446e92a0e9ffee20ec6f
Jan Saris <jan.saris@gmail.com>
2019-07-05 14:22:52 +0200

    Extract text from image

--- ConvertVideo/Program.cs
+++ ConvertVideo/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Tesseract;
 
 namespace ConvertVideo
 {
@@ -11,7 +13,6 @@ namespace ConvertVideo
         private const string TimeFormat = @"hh\:mm\:ss";
 
         private FileInfo _input;
-        private FileInfo _output;
         private DirectoryInfo _inputFolder;
         private DirectoryInfo _outputFolder;
         private Settings _settings;
@@ -31,6 +32,7 @@ namespace ConvertVideo
             try
             {
                 Logger.Info("Welcome to movie converter!");
+                Logger.Info(ExtractTitle(@"E:\GitHub\ConvertVideo\Test\vlc_2019-07-05_11-19-08.png"));
                 var program = new Program();
                 program.Initialize(args);
                 program.Run().Wait();
@@ -78,10 +80,9 @@ namespace ConvertVideo
             foreach (var file in _inputFolder.GetFiles())
             {
                 _input = file;
-                var inputNameWithoutExtension = _input.Name.Replace(_input.Extension, "");
-                _output = new FileInfo($"{_outputFolder.FullName}\\{inputNameWithoutExtension}.mp4");
+                var output = GetOutputFileAs("mp4");
                 if (FilterOnFileName(file.Name)) continue;
-                if (!ValidOutput(_output)) continue;
+                if (!ValidOutput(output)) continue;
                 Logger.Info($"Start conversion of {_input.FullName}");
 
                 var startFrames = await FindKeyFrame(_settings.StartImageFile);
@@ -98,10 +99,32 @@ namespace ConvertVideo
                     endFrame = (startFrames.Value.frame + _settings.DefaultVideoLengthInSeconds * _settings.FramesPerSecond, 0);
                 }
 
-                await CreateThumbnail(startFrames.Value);
-                //await ConvertVideo(startFrames.Value.keyframe, endFrame.Value.frame);
+                var thumbnail = await CreateThumbnail(startFrames.Value);
+                var title = ExtractTitle(thumbnail);
+                await ConvertVideo(startFrames.Value.keyframe, endFrame.Value.frame, output.FullName);
+                CreateNfo(title);
                 Logger.Info($"Finished conversion of {_input.FullName}");
             }
+        }
+
+        private FileInfo GetOutputFileAs(string extension)
+        {
+            var inputNameWithoutExtension = _input.Name.Replace(_input.Extension, "");
+            return new FileInfo($"{_outputFolder.FullName}\\{inputNameWithoutExtension}.{extension}");
+        }
+
+        private void CreateNfo(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string ExtractTitle(string thumbnail)
+        {
+            var pix = Pix.LoadFromFile(thumbnail);
+            var engine = new TesseractEngine(".", "nld", EngineMode.Default);
+            //x400 y530 w600 h70
+            var page = engine.Process(pix, new Rect(400, 530, 600, 70), PageSegMode.SingleLine);
+            return page.GetText();
         }
 
         private async Task ListenForKeyboardInput()
@@ -129,11 +152,11 @@ namespace ConvertVideo
 
             if (_settings.SkipFileIfOutputExists)
             {
-                Logger.Info($"Output file '{_output.FullName}' already exists, skip this one");
+                Logger.Info($"Output file '{output.FullName}' already exists, skip this one");
                 return false;
             }
-            Logger.Info($"Output file '{_output.FullName}' already exists, delete it first");
-            _output.Delete();
+            Logger.Info($"Output file '{output.FullName}' already exists, delete it first");
+            output.Delete();
 
             return true;
         }
@@ -153,20 +176,22 @@ namespace ConvertVideo
             return await _ffmpeg.FindFrameByImage(_input.FullName, image);
         }
 
-        private async Task CreateThumbnail((int frame, int keyframe) startFrames)
+        private async Task<string> CreateThumbnail((int frame, int keyframe) startFrames)
         {
-            if (_token.IsCancellationRequested) return;
+            if (_token.IsCancellationRequested) return null;
             var frameNumber = startFrames.frame + _settings.ThumbnailTakenInFramesAfterStart;
             var start = TimeSpan.FromSeconds((double)frameNumber / 25).ToString(TimeFormat);
-            await _ffmpeg.ExtractImage(_input.FullName, $"{_output.FullName.Replace(_output.Extension,"")}.jpg", start);
+            var thumbnail = GetOutputFileAs("jpg");
+            await _ffmpeg.ExtractImage(_input.FullName, thumbnail.FullName, start);
+            return thumbnail.FullName;
         }
 
-        private async Task ConvertVideo(int startFrame, int endFrame)
+        private async Task ConvertVideo(int startFrame, int endFrame, string output)
         {
             if (_token.IsCancellationRequested) return;
             var start = TimeSpan.FromSeconds((double)startFrame / 25).ToString(TimeFormat);
             var end = TimeSpan.FromSeconds((double)(endFrame - startFrame) / 25).ToString(TimeFormat);
-            await _ffmpeg.Convert(_input.FullName, _output.FullName, start, end);
+            await _ffmpeg.Convert(_input.FullName, output, start, end);
         }
     }
 }
